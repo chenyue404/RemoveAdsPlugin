3539c5f9efcac09f890046fc1178fa94637bf429
chenyue404 <601450225@qq.com>
2019-07-11 11:20:18 +0800

    修复列表广告bug 去除开屏广告

--- app/src/main/java/com/chenyue/cancelAds/hook/WeiboHook.kt
+++ app/src/main/java/com/chenyue/cancelAds/hook/WeiboHook.kt
@@ -2,6 +2,7 @@ package com.chenyue.cancelAds.hook
 
 import de.robv.android.xposed.IXposedHookLoadPackage
 import de.robv.android.xposed.XC_MethodHook
+import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.XposedHelpers.findAndHookMethod
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 
@@ -22,21 +23,61 @@ class WeiboHook : IXposedHookLoadPackage {
             return
         }
 
+        val StatusClass = XposedHelpers.findClass("com.weico.international.model.sina.Status", classLoader);
         findAndHookMethod("com.weico.international.utility.KotlinExtendKt",
             classLoader,
             "isWeiboUVEAd",
+            StatusClass,
             object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     param.result = false
                 }
             })
 
+        val PageInfo = XposedHelpers.findClass("com.weico.international.model.sina.PageInfo", classLoader);
         findAndHookMethod("com.weico.international.utility.KotlinUtilKt",
             classLoader,
             "findUVEAd",
+            PageInfo,
             object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     param.result = null
+                }
+            })
+
+//        findAndHookMethod("com.weico.international.activity.NewSplashActivity",
+//            classLoader,
+//            "requestSplashAd",
+//            object : XC_MethodHook() {
+//                override fun beforeHookedMethod(param: MethodHookParam) {
+//                    Log.e("cancelAds", "requestSplashAd")
+//                    XposedHelpers.callMethod(
+//                        "com.weico.international.activity.NewSplashActivity",
+//                        "next"
+//                    );
+//                    param.result = null
+//                }
+//            })
+//
+//        val FlashAdBuilder = XposedHelpers.findClass("com.weibo.mobileads.controller.Builder.FlashAdBuilder", classLoader);
+//        findAndHookMethod("com.weibo.mobileads.controller.AdSdk",
+//            classLoader,
+//            "initFlashAd",
+//            FlashAdBuilder,
+//            object : XC_MethodHook() {
+//                override fun beforeHookedMethod(param: MethodHookParam) {
+//                    Log.e("cancelAds", "initFlashAd")
+//                    param.result = null
+//                }
+//            })
+
+
+        findAndHookMethod("com.weibo.mobileads.view.FlashAd",
+            classLoader,
+            "isReady",
+            object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    param.result = false
                 }
             })
 
